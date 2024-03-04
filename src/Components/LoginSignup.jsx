1b5729f630facc2a16663992182c2cc74838c227
Kevin <kevin.lucassg@gmail.com>
2024-03-03 23:54:58 -0300

    Initial commit

---
+++ src/Components/LoginSignup.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./LoginSignup.css";
+
+export default function LoginSignup() {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Name</div>
+        <input type="text" />
+        <div className="text">Email</div>
+        <input type="email" />
+        <div className="text">Password</div>
+        <input type="password" />
+      </div>
+    </div>
+  );
+}
