f0c62b1374f95cda40bfe16b95cf56d2b8da6404
Kevin <kevin.lucassg@gmail.com>
2024-03-04 23:57:16 -0300

    Updated Login Page

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -5,12 +5,14 @@ export default function LoginSignup() {
   return (
     <div className="container">
       <div className="header">
+        <div className="login">Login</div>
         <div className="text">Name</div>
         <input type="text" />
         <div className="text">Email</div>
         <input type="email" />
         <div className="text">Password</div>
         <input type="password" />
+        <button>Login</button>
       </div>
     </div>
   );
