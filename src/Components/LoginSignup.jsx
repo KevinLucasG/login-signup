65a26a1fb3c1126df3a0f8438c3d1c2e59db80f9
Kevin <kevin.lucassg@gmail.com>
2024-03-05 00:21:38 -0300

    Improved the appearance of the Login page

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -6,13 +6,16 @@ export default function LoginSignup() {
     <div className="container">
       <div className="header">
         <div className="login">Login</div>
-        <div className="text">Name</div>
+        <div className="text">Username</div>
         <input type="text" />
         <div className="text">Email</div>
         <input type="email" />
         <div className="text">Password</div>
         <input type="password" />
-        <button>Login</button>
+        <div className="section">
+          <button>Login</button>
+          <div className="signup">Or Sign Up</div>
+        </div>
       </div>
     </div>
   );
