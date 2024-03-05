d0c82b1be69a0908a6af734a1c019e568ed73f14
Kevin <kevin.lucassg@gmail.com>
2024-03-05 20:30:40 -0300

    Corrected the position of the div and improved texts and inputs

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -7,11 +7,11 @@ export default function LoginSignup() {
       <div className="header">
         <div className="login">Login</div>
         <div className="text">Username</div>
-        <input type="text" />
+        <input type="text" placeholder="Typer your username" />
         <div className="text">Email</div>
-        <input type="email" />
+        <input type="email" placeholder="Type your email" />
         <div className="text">Password</div>
-        <input type="password" />
+        <input type="password" placeholder="Type your password" />
         <div className="section">
           <button>Login</button>
           <div className="signup">Or Sign Up</div>
