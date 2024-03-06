0c85cdcde4c379747d7105cdfb7de07220f35c84
Kevin <kevin.lucassg@gmail.com>
2024-03-06 20:21:00 -0300

    Implemented dynamic function between Login and Signup

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LoginSignup.css";
-import user_icon from "./Assets/person.png";
 
 export default function LoginSignup() {
+  const [action, setAction] = useState("Sign Up");
   return (
     <div className="container">
       <div className="header">
-        <div className="login">Login</div>
+        <div className="title">{action}</div>
         <div className="text">Username</div>
         <div className="inputs"></div>
         <input type="text" placeholder="Typer your username" id="userIcon" />
@@ -20,8 +20,22 @@ export default function LoginSignup() {
           id="passwordIcon"
         />
         <div className="section">
-          <button>Login</button>
-          <div className="signup">Or Sign Up</div>
+          <button
+            className={action === "Login" ? "submit" : "subimit"}
+            onClick={() => {
+              setAction("Sign Up");
+            }}
+          >
+            Login
+          </button>
+          <div
+            className={action === "Sign Up" ? "signup" : "signup"}
+            onClick={() => {
+              setAction("Login");
+            }}
+          >
+            Or Sign Up
+          </div>
         </div>
       </div>
     </div>
