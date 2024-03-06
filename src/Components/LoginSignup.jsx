17815bb23b44d46f9756c6cf48c59dd378d9ad26
Kevin <kevin.lucassg@gmail.com>
2024-03-05 23:47:55 -0300

    Assigned icons to fields

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./LoginSignup.css";
+import user_icon from "./Assets/person.png";
 
 export default function LoginSignup() {
   return (
@@ -7,11 +8,17 @@ export default function LoginSignup() {
       <div className="header">
         <div className="login">Login</div>
         <div className="text">Username</div>
-        <input type="text" placeholder="Typer your username" />
+        <div className="inputs"></div>
+        <input type="text" placeholder="Typer your username" id="userIcon" />
         <div className="text">Email</div>
-        <input type="email" placeholder="Type your email" />
+        <input type="email" placeholder="Type your email" id="emailIcon" />
         <div className="text">Password</div>
-        <input type="password" placeholder="Type your password" />
+
+        <input
+          type="password"
+          placeholder="Type your password"
+          id="passwordIcon"
+        />
         <div className="section">
           <button>Login</button>
           <div className="signup">Or Sign Up</div>
